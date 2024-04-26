d2d370248a5309f3058d50831d8afc0861ba70c5
Pawan Chandra Joshi <pawanpcj600@gmail.com>
2024-04-26 11:55:21 +0530

    Initial Commit

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const { ApolloServer } = require('@apollo/server')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const { expressMiddleware } = require('@apollo/server/exp')
+
+async function startServer() {
+    const app = express();
+    const server = new ApolloServer({});
+
+    app.use(bodyParser.json())
+    app.use(cors())
+
+    await server.start()
+
+    app.use('/graphql', expressMiddleware(server))
+}
